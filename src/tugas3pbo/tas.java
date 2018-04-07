8cd7ea5992985ca5cf71caade80cfba5802cc8b8
desyfajrianti <dessy.kps@gmail.com>
2018-04-07 16:14:13 +0800

    buat method cetakinfo()

--- src/tugas3pbo/tas.java
+++ src/tugas3pbo/tas.java
@@ -4,4 +4,10 @@ public class tas {
     String merk ;
     String warna;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("warna \t: "+warna+"\n"+
+                "merk \t: "+merk+"\n"+"harga \t: "+harga+"\n");
+    }
+    
 }
