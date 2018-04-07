5d43bf1ea34da75e8860f8d63de8aab75a73e22f
desyfajrianti <dessy.kps@gmail.com>
2018-04-07 16:30:43 +0800

    buat getter and setter

--- src/tugas3pbo/tas.java
+++ src/tugas3pbo/tas.java
@@ -9,5 +9,29 @@ public class tas {
         System.out.println("warna \t: "+warna+"\n"+
                 "merk \t: "+merk+"\n"+"harga \t: "+harga+"\n");
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
     
 }
