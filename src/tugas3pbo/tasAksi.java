808bd997e33221d9803747ae9914f25dd5a2d196
desyfajrianti <dessy.kps@gmail.com>
2018-04-07 16:25:32 +0800

    instance buat atribut dan panggil method

--- src/tugas3pbo/tasAksi.java
+++ src/tugas3pbo/tasAksi.java
@@ -1,7 +1,12 @@
 package tugas3pbo;
 public class tasAksi {
     public static void main(String[] args) {
+        tas t = new tas();
+        t.warna="ungu";
+        t.merk="Alto";
+        t.harga=500;
         
+        t.cetakInfo();
     }
     
 }
