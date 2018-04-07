c3599e62b76cb2b88eaa091c243f1f55a3395d85
desyfajrianti <dessy.kps@gmail.com>
2018-04-07 16:33:12 +0800

    menggunakan method set

--- src/tugas3pbo/tasAksi.java
+++ src/tugas3pbo/tasAksi.java
@@ -2,9 +2,9 @@ package tugas3pbo;
 public class tasAksi {
     public static void main(String[] args) {
         tas t = new tas();
-        t.warna="ungu";
-        t.merk="Alto";
-        t.harga=500;
+        t.setWarna("biru");
+        t.setMerk("Alto");
+        t.setHarga(500);
         
         t.cetakInfo();
     }
